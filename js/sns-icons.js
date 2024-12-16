bc3d13006a444bdb1b52550d7ccc9b0923f585de
apodteamgo <apodteam@gmail.com>
2024-12-17 00:11:49 +0900

    Update sns-icons.js

--- js/sns-icons.js
+++ js/sns-icons.js
@@ -1,11 +1,15 @@
-const callout = document.querySelector('.notion-callout');
-const linkElement = callout.querySelector('.notion-callout__content a');
-const iconElement = callout.querySelector('.notion-callout__icon img');
+document.querySelectorAll('.notion-callout').forEach((callout) => {
+  const linkElement = callout.querySelector('.notion-callout__content a'); // 링크 요소
+  const iconElement = callout.querySelector('.notion-callout__icon img'); // 아이콘 요소
 
-if (linkElement && iconElement) {
-  callout.querySelector('.notion-callout__icon').addEventListener('click', () => {
-    window.open(linkElement.href, '_blank');
-  });
+  if (linkElement && iconElement) {
+    // 아이콘 클릭 이벤트 설정
+    callout.querySelector('.notion-callout__icon').addEventListener('click', () => {
+      window.open(linkElement.href, '_blank'); // 새 탭에서 링크 열기
+    });
 
-  iconElement.style.cursor = 'pointer';
-}
+    // 커서 모양 변경
+    iconElement.style.cursor = 'pointer';
+  }
+});
+
